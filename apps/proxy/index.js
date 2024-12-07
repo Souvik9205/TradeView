ecfcf22064207bc9a8c064283859532c0b7949b7
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-12-07 20:36:38 +0530

    proxy cors

--- apps/proxy/index.js
+++ apps/proxy/index.js
@@ -7,7 +7,7 @@ const targetUrl = "https://api.backpack.exchange";
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://marketview-rust.vercel.app"
+    "https://marketview-rust.vercel.app/"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -22,7 +22,7 @@ app.use(
     target: targetUrl,
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
-      proxyReq.setHeader("origin", "https://marketview-rust.vercel.app");
+      proxyReq.setHeader("origin", "https://marketview-rust.vercel.app/");
       proxyReq.setHeader("Authorization", "Bearer <your-api-token>");
     },
     onProxyRes: (proxyRes, req, res) => {
